1687ce2452bc208cf42ef0e1a843e58128654fbb
Masood <masood@entrayn.com>
2018-10-28 15:28:25 +0530

    At post #2

---
+++ objects/Test.lua
@@ -0,0 +1,15 @@
+Test = Object:extend()
+
+function Test:new(  )
+  -- body
+end
+
+function Test:update(dt)
+  -- body
+end
+
+function Test:draw(  )
+  -- body
+  love.graphics.draw(image, love.math.random(0,800), love.math.random(0, 600))
+end
+
